6efacc15d5aa92fe3a010e995ca29ec06a97bd5f
ranatamerr <roneytamer@gmail.com>
2026-02-02 16:22:08 +0200

    Add TaskItem model with validation and timestamps

---
+++ Models/TaskItem.cs
@@ -0,0 +1,19 @@
+using System.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+
+    public DateTime DueDate { get; set; }
+    public TaskPriority Priority { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }= DateTime.UtcNow;
+}
