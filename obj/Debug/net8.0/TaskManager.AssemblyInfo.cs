a2740413df5837ad9c3022cbac8b5b434d2414c4
ranatamerr <roneytamer@gmail.com>
2026-02-02 15:35:34 +0200

    Add Entity Framework Core and SQLite packages

--- obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a29e60dd14d94d99b990299f6c7b374b0e0b8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566b1343603d5484ed53aaa93685c6914c46ca75")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
